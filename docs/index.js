0fdf70f78267f437366fc9f64e4d33f1a0434d3f
Daniel Davidson <danielmcraedavidson@gmail.com>
2024-03-05 14:39:35 -0600

    rearranged html layout and text animation completed

--- docs/index.js
+++
@@ -1,6 +0,0 @@
-document.getElementsByClassName("copy").onclick = function() {
-    navigator.clipboard.writeText(document.getElementById("email").innerText)
-    .then(function() {
-        console.log("text copied!!!!!!!!");
-    });
-};
